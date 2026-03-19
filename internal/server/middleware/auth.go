07703ef7e96b10ba8f257d4a15ed03c128420dbe
tjlcast <phx108@sina.com>
2026-03-19 17:25:18 +0800

    feat: remove apikey prefix validation

--- internal/server/middleware/auth.go
+++ internal/server/middleware/auth.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/bestruirui/octopus/internal/conf"
 	"github.com/bestruirui/octopus/internal/op"
 	"github.com/bestruirui/octopus/internal/server/auth"
 	"github.com/bestruirui/octopus/internal/server/resp"
@@ -56,11 +55,11 @@ func APIKeyAuth() gin.HandlerFunc {
 			return
 		}
 
-		if !strings.HasPrefix(apiKey, "sk-"+conf.APP_NAME+"-") {
-			resp.Error(c, http.StatusUnauthorized, resp.ErrUnauthorized)
-			c.Abort()
-			return
-		}
+		// if !strings.HasPrefix(apiKey, "sk-"+conf.APP_NAME+"-") {
+		// 	resp.Error(c, http.StatusUnauthorized, resp.ErrUnauthorized)
+		// 	c.Abort()
+		// 	return
+		// }
 		apiKeyObj, err := op.APIKeyGetByAPIKey(apiKey, c.Request.Context())
 		if err != nil {
 			resp.Error(c, http.StatusUnauthorized, resp.ErrUnauthorized)
